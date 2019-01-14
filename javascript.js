f7cad08dec9dadca8c09744eefcb2b5f8974451a
KevinLe <kevinles117@yahoo.com>
2019-01-13 23:04:58 -0800

    Please

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
-function myFunction() {
-  alert("I am an alert");
+function getDate() {
+var dt = new Date();
+alert(("0"+(dt.getMonth()+1)).slice(-2)) +"/"+ (("0"+dt.getDate()).slice(-2)) +"/"+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()+1).slice(-2)) +":"+ (("0"+dt.getMinutes()+1).slice(-2));
 }
