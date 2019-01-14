4e67455992ebfb13a8b8af3f20580092b5754d8f
KevinLe <kevinles117@yahoo.com>
2019-01-13 23:26:04 -0800

    eifj;osej

--- javascript.js
+++ javascript.js
@@ -1,12 +1,11 @@
-function getDate() {
-var dt = new Date(unix_timestamp*1000);
+function getDateandTime() {
+var dt = new Date();
 
-var hours = dt.getHours();
-
-var minutes = "0" + dt.getMinutes();
-
-var seconds = "0" + dt.getSeconds();
-
-var formattedTime = hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
-alert(getDate());
+var formattedTime = dt.getDate() + "/"
+                + (dt.getMonth() + 1)  + "/"
+                + dt.getFullYear() + " @ "
+                + dt.getHours() + ":"
+                + dt.getMinutes() + ":"
+                + dt.getSeconds();
+alert(formattedTime);
 }
